7cd3a8ae9a2ce2450bccdadb19818d8622e18b78
renatorocketdev <renatorocketdev@gmail.com>
2020-09-29 08:40:56 -0300

    Files Management

---
+++ documents/script.sql
@@ -0,0 +1,10 @@
+--Table Enterprises
+CREATE TABLE Enterprises
+(
+  enterprisesId INT NOT NULL,
+  name VARCHAR(70) NOT NULL,
+  icon VARBINARY(MAX) NOT NULL,
+  address VARCHAR(255),
+  contact VARCHAR(15),
+  
+);
